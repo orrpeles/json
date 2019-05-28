35b156e0031da4ac3a5d70bcd7e45d9bc3b08bc7
orrpeles <orr.backup37@gmail.com>
2019-05-28 17:02:15 +0300

    modified quickweather

--- quickWeather.py
+++ quickWeather.py
@@ -1,15 +1,22 @@
 #!/bin/python
 # Fetch weather data from command line using JSON
 
-import sys, json, requests
+import sys, json, requests, pprint
+
 # read requested location from command line
 if len(sys.argv) < 2:
     print('enter a location to fetch weather from...')
     sys.exit()
-location = ' '.join(sys.argv[1:]) #join strings except the first
+location = ','.join(sys.argv[1:]) #join strings except the first
 
-url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+url = 'https://api.openweathermap.org/data/2.5/weather?q=%s&APPID=aaa7e7b779dfa16b4073042d2a2d3047' % (location)
 response = requests.get(url)
 response.raise_for_status()
 
+# print(response.text)
+
+weatherData = json.loads(response.text)
+pprint.pprint(weatherData)
+
+#w = weatherData['list']
 
